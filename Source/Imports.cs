b794b3e29eddc579ac867645a96c757dccda0690
kzh <103158896+kzh1337@users.noreply.github.com>
2022-05-18 16:40:25 -0500

    Update and rename Imports to Imports.cs

---
+++ Source/Imports.cs
@@ -0,0 +1,27 @@
+
+public static class Imports
+    {
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT IpRect);
+
+        public static RECT rect;
+
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+    }
